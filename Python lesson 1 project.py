4fa3d3601b6c4cb8a7ad7b59935ecb4876148fdb
Murtazaanjum56 <Murtazaanjum09@gmail.com>
2025-06-12 15:06:21 +0400

    Add files via upload

---
+++ Python lesson 1 project.py
@@ -0,0 +1,6 @@
+print("Hello")
+# I will show if a number is odd or not.
+print("The number", 3 + 2, "is an odd number.")
+print("However, the number", 3 + 3, "is an even number.")
+print("Even numbers are always multiples of 2, for example", 3 * 4, "is an"
+"even number.")
