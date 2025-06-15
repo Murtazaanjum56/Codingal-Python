56df1f9b65c9d425969e062bb81879d388e66166
Murtazaanjum56 <Murtazaanjum09@gmail.com>
2025-06-15 14:29:37 +0400

    Add files via upload

---
+++ Data Types PT1.py
@@ -0,0 +1,25 @@
+# This is for Mean
+Num1 = 5
+Num2 = 4
+Num3 = 3
+Num4 = 2
+Num5 = 1
+Add = Num1 + Num2 + Num3 + Num4 + Num5
+div = Add / 5
+print("The mean of the 5 numbers is: ", div)
+
+# This is for the conversion of celsius to fahrenheit
+
+Cel = 4
+Far = (Cel * 33.8)
+print("If the celsius is: ", Cel, "then the farenheit is: ", Far)
+
+# This is for the perimeter of a circle
+
+Rad = 5
+Dia = Rad * 2
+Pi = 3.14
+Per = Pi * Dia
+Area = Pi * Rad * Rad
+print("The perimter of a circle is diamter * pi: ", Per)
+
